3b67b51ad2cbbb4c80e4f095a52507405decb137
Alexandra1123 <al.h8500@gmail.com>
2025-03-11 00:00:57 +0400

    Corrección para notificaciones internas y de otros servidores

--- src/middleware/notification.ts
+++ src/middleware/notification.ts
@@ -13,41 +13,46 @@ export const saveNotificationMiddleware = (
   let notificationSaved = false;
 
   res.send = function (body: any) {
-    if (
-      !notificationSaved &&
-      res.statusCode >= 200 &&
-      res.statusCode < 300 &&
-      req.body
-    ) {
-      notificationSaved = true;
-      const notification: NotificationI = req.body.notification;
-
-      // Certificación enviada por 1ra vez
+    try {
       if (
-        notification.action == NotificationAction.VIEW_CERTIFICATION &&
-        !notification.targetId
+        !notificationSaved &&
+        res.statusCode >= 200 &&
+        res.statusCode < 300 &&
+        req.body
       ) {
-        notification.targetId = body.res?.uid;
+        notificationSaved = true;
+        const notification: NotificationI =
+          req.body.notification ?? body.notification;
+
+        // Certificación enviada por 1ra vez
+        if (
+          notification.action == NotificationAction.VIEW_CERTIFICATION &&
+          !notification.targetId
+        ) {
+          notification.targetId = body.res?.uid;
+        }
+
+        if (notification.receiverId && notification.targetId) {
+          console.log(notification, body);
+          NotificationModel.create(notification)
+            .then((res) => {
+              io.to(`notification${notification.receiverId}`).emit(
+                "updateRoom",
+                {
+                  dataPack: { data: [res.toObject()] },
+                  typeSocket: TypeSocket.CREATE,
+                  key: res.uid,
+                  userId: res.senderId,
+                }
+              );
+            })
+            .catch((error) =>
+              console.error("Error saving notification:", error)
+            );
+        }
       }
-      console.log(
-        "========",
-        notification,
-        notification.receiverId,
-        notification.targetId
-      );
-      if (notification.receiverId && notification.targetId) {
-        console.log(notification, body);
-        NotificationModel.create(notification)
-          .then((res) => {
-            io.to(`notification${notification.receiverId}`).emit("updateRoom", {
-              dataPack: { data: [res.toObject()] },
-              typeSocket: TypeSocket.CREATE,
-              key: res.uid,
-              userId: res.senderId,
-            });
-          })
-          .catch((error) => console.error("Error saving notification:", error));
-      }
+    } catch (e) {
+      console.log("Error al crear notificación", e);
     }
 
     return originalSend(body);
