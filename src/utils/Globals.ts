f2822c4a8d5f5feaf5e0ecd8211ef93705fb85da
al.h8500@gmail.com <al.h8500@gmail.com>
2025-03-18 17:35:24 -0500

    45 min para notificación de rubros

--- src/utils/Globals.ts
+++ src/utils/Globals.ts
@@ -1,4 +1,4 @@
 export const notificationSystemSenderId = "1";
 export const notificationSystemSenderName = "TCompra";
-export const broadcastNotificationExpiresIn = 60; // 1 hora
+export const broadcastNotificationExpiresIn = 45; // minutos
 export const directNotificationExpiresIn = 30; // 30 días
